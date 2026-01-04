2e0eb08773316cafa51459e4d7f05a1bdc6c2583
andreyKAR <your_emailandrey1999karas@gmail.com>
2026-01-04 01:25:48 +0100

    tasks

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,38 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import '../css/snackbar.css';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'OK',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        // timeout: 0,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: 'Illegal operation',
+        position: 'topRight',
+        iconUrl: '../img/bi_x-octagon (1).svg',
+        // timeout: 0,
+      });
+    });
+});
